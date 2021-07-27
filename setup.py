b9843fbcb1a315bd8cd3cf9177b05a00d6f5a70a
Ric Domingues <Ric.Domingues@dtn.com>
2021-07-27 14:12:15 -0400

    upd setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rd_datasci_pytools',
+      version='1.0.0',
+      description='Collection of python functions',
+      url='https://github.com/ric-domingues/rd_datasci_pytools',
+      author='Ricardo Domingues',
+      author_email='ricardormd@gmail.com',
+      license='GNU',
+      packages=['rd_datasci_pytools'],
+      zip_safe=False)
